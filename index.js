31bf1ee8f37584183a92264b9c69eb26d0bde455
Casey Zimmer <czimmer@ramapo.edu>
2018-08-08 01:17:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function theBeatlesPlay(musicians,instruments)
   
   for(let i = 0; i < musicians.length; i++)
   {
-    temp.push(musicians[i] +  "plays" + instruments[i]);
+    temp.push(musicians[i] +  "plays " + instruments[i]);
   }
   
   return temp;
